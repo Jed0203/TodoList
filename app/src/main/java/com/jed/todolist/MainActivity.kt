ea9d05c266ac64ae9c98eee32261958ff3a0128c
jed-chang <cha19057@byui.edu>
2021-06-19 16:39:11 -0600

    Todo List app

---
+++ app/src/main/java/com/jed/todolist/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jed.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
